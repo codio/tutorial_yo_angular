7baaee5aa035729d4c3a6f1ab662b5e23d1ad7ec
fmay <fmay@applicationcraft.com>
2014-02-27 19:57:31 +0000

    base position

--- README.md
+++
@@ -1,4 +0,0 @@
-# Hello world!
-
-This is a placeholder README file for your project. Update it with information about your project,
-so others can find out more.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('workspaceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
